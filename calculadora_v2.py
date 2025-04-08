ba902bb2180c1e7a85eda1c2788158ad9504d053
“a-vecchi” <alex_vecchi@hotmail.com>
2025-04-08 12:14:30 -0300

    Trabalho Prático: Calculadora

---
+++ calculadora_v2.py
@@ -0,0 +1,37 @@
+saida = ''
+
+def adicao(x, y):
+    return float(x) + float(y)
+
+def subracao(x, y):
+    return float(x) - float(y)
+
+def multiplicacao(x, y):
+    return float(x) * float(y)
+
+def divisao(x, y):
+    if float(x) == 0 or float(y) == 0:
+        return 'Não foi possível realizar a divisão por 0'
+    else:
+        return float(x) / float(y)
+
+def calculadora(numero1, numero2, operacao):
+    if operacao == '+' or operacao == 'adicao':
+        resultado = adicao(numero1, numero2)
+    elif operacao == '-' or operacao == 'subtracao':
+        resultado = subracao(numero1, numero2)
+    elif operacao == '*' or operacao == 'multiplicacao':
+        resultado = multiplicacao(numero1, numero2)
+    elif operacao == '/' or operacao == 'divisao':
+       resultado = divisao(numero1, numero2)
+    else:
+        resultado = 'Operação não implementada'
+    return resultado
+
+while saida.lower() != 'n':
+    numero1 = input('Digite o primeiro numero: ')
+    numero2 = input('Digite o segundo numero: ')
+    operacao = input('Digita a operação desejada: ')
+    resultado = calculadora(numero1, numero2, operacao)
+    print(f'Resultado da operação: {resultado}')
+    saida = input('Deseja continuar ou não executando o programa? (S/N):')
